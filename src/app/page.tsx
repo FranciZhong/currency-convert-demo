a741b3856eec63337d36379f369056f39c67d7a7
FranciZhong <francis.zhichao@gmail.com>
2025-05-07 20:51:19 +1000

    fix: api error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
 import CurrencyConvert from "@/components/currency-convert";
+import ErrorCard from "@/components/error-card";
 import { getLatestRates } from "@/lib/api/open-exchange-api";
-
 export default async function Home() {
-  const latestRates = await getLatestRates();
+  try {
+    const latestRates = await getLatestRates();
 
-  return (
-    <div className="h-min-[100dvh] flex justify-center">
-      <div className="container flex max-w-2xl flex-col items-center gap-4 px-8">
-        <h1 className="bg-background sticky top-0 py-4 text-left text-2xl font-bold">
-          Currency Converter
-        </h1>
-        <CurrencyConvert latestRates={latestRates} />
+    return (
+      <div className="h-min-[100dvh] flex justify-center">
+        <div className="container flex max-w-2xl flex-col items-center gap-4 px-8">
+          <h1 className="bg-background sticky top-0 py-4 text-left text-2xl font-bold">
+            Currency Converter
+          </h1>
+          <CurrencyConvert latestRates={latestRates} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error(error);
+    return (
+      <div className="flex h-[100dvh] items-center justify-center">
+        <ErrorCard>
+          <p className="text-lg font-semibold">Failed to fetch latest rates.</p>
+          <p className="text-muted-foreground font-light">
+            Please try again later.
+          </p>
+        </ErrorCard>
+      </div>
+    );
+  }
 }
